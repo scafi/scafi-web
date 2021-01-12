bb708962cfba2a3f6822e9a4f096c05add962eef
Niccolò Maltoni <nicco.mlt@gmail.com>
2021-01-12 17:55:32 +0100

    refactor: start learning the codebase and apply Scalastyle/IDEA refactors

--- scafi-web/src/main/scala/it/unibo/scafi/js/Index.scala
+++ scafi-web/src/main/scala/it/unibo/scafi/js/Index.scala
@@ -2,88 +2,113 @@ package it.unibo.scafi.js
 
 import it.unibo.scafi.js.controller.local
 import it.unibo.scafi.js.controller.local._
+import it.unibo.scafi.js.dsl.{BasicWebIncarnation, ScafiInterpreterJs, WebIncarnation}
 import it.unibo.scafi.js.dsl.semantics._
-import it.unibo.scafi.js.dsl.{ScafiInterpreterJs, WebIncarnation}
 import it.unibo.scafi.js.utils.{Cookie, Execution}
 import it.unibo.scafi.js.view.dynamic._
-import it.unibo.scafi.js.view.dynamic.graph.LabelRender._
 import it.unibo.scafi.js.view.dynamic.graph.{PhaserGraphSection, PhaserInteraction}
+import it.unibo.scafi.js.view.dynamic.graph.LabelRender._
 import it.unibo.scafi.js.view.static.{RootStyle, SkeletonPage}
+import monix.execution.Scheduler
 import org.scalajs.dom.experimental.URLSearchParams
 
 import java.util.concurrent.TimeUnit
 import scala.concurrent.duration.FiniteDuration
 import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
+
 /**
-  * Root object, it initialize the simulation, the page and the backend.
-  */
+ * Root object, it initialize the simulation, the page and the backend.
+ */
 object Index {
+
   import org.scalajs.dom._
-  implicit val incarnation = WebIncarnation //incarnation choosed
+
+  implicit val incarnation: BasicWebIncarnation = WebIncarnation // incarnation chosen
+
+  /**
+   * Interpreter chosen.
+   */
   @JSExportTopLevel("Lang")
-  implicit val languageJsInterpreter = new ScafiInterpreterJs("Lang") with BlockGJs
-    with LanguageJs with BlockTJs with StandardSensorJs with BuiltinsJs// interpreter choosen
-  val configuration = SupportConfiguration(
+  implicit val languageJsInterpreter: ScafiInterpreterJs[BasicWebIncarnation] =
+  new ScafiInterpreterJs("Lang")
+    with BlockGJs
+    with LanguageJs
+    with BlockTJs
+    with StandardSensorJs
+    with BuiltinsJs
+
+  val configuration: SupportConfiguration = SupportConfiguration(
     GridLikeNetwork(10, 10, 60, 60, 0),
     SpatialRadius(range = 70),
     deviceShape = DeviceConfiguration.standard,
     seed = SimulationSeeds(),
   )
-  val updateTime = 50 //todo think to put into a configuration
+
+  val updateTime = 50 // todo think to put into a configuration
+
   @JSExportTopLevel("Platform")
   val support = new SimulationSupport(configuration)
     with SimulationExecutionPlatform
     with SimulationCommandInterpreter
 
-  lazy val editor = EditorSection(SkeletonPage.editorSection, SkeletonPage.selectionProgram, SkeletonPage.modeSelector, programs)
+  lazy val editor: EditorSection =
+    EditorSection(SkeletonPage.editorSection, SkeletonPage.selectionProgram, SkeletonPage.modeSelector, programs)
 
   @JSExport
   def main(args: Array[String]): Unit = {
     val queryParams = new URLSearchParams(document.location.search)
-    if (queryParams.has("headless")) { contentOnly() } else { spaPage() }
+    if (queryParams.has("headless")) {
+      contentOnly()
+    } else {
+      spaPage()
+    }
     scafiInitialization()
   }
+
   lazy val programs = Map(
     "round counter" -> "return rep(() => 0, (k) => k+1)",
     "hello scafi" -> "return \"hello scafi\"",
-    "gradient" -> """return rep(() => Infinity, (d) => {
+    "gradient" ->
+      """return rep(() => Infinity, (d) => {
         |  return mux(sense("source"), 0.0,
         |    foldhoodPlus(() => Infinity, Math.min, () => nbr(() => d) + nbrvar("nbrRange"))
         |  )
         | })
         |""".stripMargin,
-    "channel" -> """var metric = () => nbrRange()
-                   |
-                   |function channel(source, target, width) {
-                   |  	var threshold = distanceBetween(source, target, metric) + width
-                   | 	return distanceTo(source, metric) + distanceTo(target, metric) < threshold
-                   |}
-                   |return channel(sense("source"), sense("obstacle"), 1)""".stripMargin
+    "channel" ->
+      """var metric = () => nbrRange()
+        |
+        |function channel(source, target, width) {
+        |  	var threshold = distanceBetween(source, target, metric) + width
+        | 	return distanceTo(source, metric) + distanceTo(target, metric) < threshold
+        |}
+        |return channel(sense("source"), sense("obstacle"), 1)""".stripMargin
   )
 
-  def spaPage() : Unit = {
+  def spaPage(): Unit = {
     document.head.appendChild(SkeletonPage.renderedStyle(RootStyle.withNav()).render)
     document.body.appendChild(SkeletonPage.fullPage.render)
     //modal.toggle()
   }
 
-  def contentOnly() : Unit = {
+  def contentOnly(): Unit = {
     //page injection
     document.head.appendChild(SkeletonPage.renderedStyle(RootStyle.withoutNav).render)
     document.body.appendChild(SkeletonPage.contentOnly.render)
   }
-  def scafiInitialization() : Unit = {
-    if(!Cookie.has("visited")) {
+
+  def scafiInitialization(): Unit = {
+    if (!Cookie.has("visited")) {
       // TODO Explanation.render(SkeletonPage.backendConfig, SkeletonPage.visualizationSection)
       val modal = Modal.textual("Welcome", "discover the power of the collective", 300)
       document.body.appendChild(modal.html)
       modal.toggle()
       Cookie.store("visited", "true")
     }
-    implicit val context = Execution.timeoutBasedScheduler
+    implicit val context: Scheduler = Execution.timeoutBasedScheduler
     //dynamic part configuration
     val visualizationSettingsSection = VisualizationSettingsSection(SkeletonPage.visualizationOptionDiv)
-    val renders : Seq[LabelRender] = Seq(BooleanRender(), BooleanExport(), /*LabelRender.gradientLike, test only*/ TextifyBitmap())
+    val renders: Seq[LabelRender] = Seq(BooleanRender(), BooleanExport(), /*LabelRender.gradientLike, test only*/ TextifyBitmap())
     val phaserRender = new PhaserGraphSection(SkeletonPage.visualizationSection, new PhaserInteraction(support), visualizationSettingsSection, renders)
     val configurationSection = new ConfigurationSection(SkeletonPage.backendConfig, support)
     SimulationControlsSection.render(support, editor, SkeletonPage.controlsDiv)
@@ -94,6 +119,7 @@ object Index {
     SkeletonPage.visualizationSection.focus()
     EventBus.publish(configuration) //tell to all component the new configuration installed on the frontend
   }
+
   @JSExportTopLevel("ScafiBackend")
   val interpreter = new local.SimulationCommandInterpreter.JsConsole(support)
 }
