600fa533c8aeec23fecaec59cf69f2bb8c88e8ce
Niccolò Maltoni <nicco.mlt@gmail.com>
2021-02-20 17:07:11 +0100

    ci: fix adjust deployment script

--- .github/scripts/adjust-deployment.sh
+++ .github/scripts/adjust-deployment.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 mkdir ./public
-for resource in basename -a ./scafi-web/src/main/resources/* ; do
+for resource in $(basename -a ./scafi-web/src/main/resources/*) ; do
   cp "./scafi-web/target/scala-2.12/classes/${resource}" ./public/ ;
 done
 mkdir -p ./public/js
