e689540403b6f5aa998f7708cb5d4d3654606504
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2022-05-27 14:39:22 +0200

    ci: fix ci

--- .github/scripts/adjust-deployment.sh
+++ .github/scripts/adjust-deployment.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 mkdir ./public
-for asset in $(basename -a ./scafi-web/src/main/resources/*) ; do
-  cp -r "./scafi-web/target/scala-2.12/classes/${asset}" ./public/ ;
+for asset in $(basename -a ./frontend/src/main/resources/*) ; do
+  cp -r "./frontend/target/scala-2.12/classes/${asset}" ./public/ ;
 done
 mkdir -p ./public/js
-cp "./scafi-web/target/scala-2.12/scalajs-bundler/main/scafi-web-opt-bundle.js" ./public/js
+cp "./frontend/target/scala-2.12/scalajs-bundler/main/frontend-opt-bundle.js" ./public/js
