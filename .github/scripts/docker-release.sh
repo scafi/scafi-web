3fbbce4d2db73c363d074e2c4e5a3169ba8920fb
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2022-05-27 17:07:09 +0200

    ci: fix tag name in script

--- .github/scripts/docker-release.sh
+++ .github/scripts/docker-release.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-cp ./online-compiler/target/scala-2.12/online-compiler.jar ./online-compiler/src/resources/online-compiler.jar
-cd ./online-compile/src/resources
-docker build -t gianlucaaguzzi/scafi-web:$GITHUB_REF .   
-docker push gianlucaaguzzi/scafi-web:$GITHUB_REF
-docker tag gianlucaaguzzi/scafi-web:$GITHUB_REF gianlucaaguzzi/scafi-web:latest 
-docker push gianlucaaguzzi/scafi-web:$GITHUB_REF
+cp ../online-compiler/target/scala-2.12/online-compiler.jar ./online-compiler/src/resources/online-compiler.jar
+cd ../online-compile/src/resources
+TAG=${GITHUB_REF##*/}
+docker build -t gianlucaaguzzi/scafi-web:$TAG .   
+docker push gianlucaaguzzi/scafi-web:$TAG
+docker tag gianlucaaguzzi/scafi-web:$TAG gianlucaaguzzi/scafi-web:latest 
+docker push gianlucaaguzzi/scafi-web:$TAG
