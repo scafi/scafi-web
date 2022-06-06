2a1ff0916e12065ad0979ce8575a704328c9d107
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2022-06-06 10:27:19 +0200

    fix(ci): fix update script

--- .github/scripts/docker-release.sh
+++ .github/scripts/docker-release.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cp ./online-compiler/target/scala-2.12/online-compiler.jar ./online-compiler/src/main/resources/onliner-compiler.jar
+cp ./online-compiler/target/scala-2.12/online-compiler.jar ./online-compiler/src/main/resources/online-compiler.jar
 cd ./online-compiler/src/main/resources
 TAG=${GITHUB_REF##*/}
 docker build -t gianlucaaguzzi/scafi-web:$TAG .   
